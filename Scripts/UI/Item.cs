d1d66c7feeef30be57d4a086a9980c01b3a06344
dzh19961003 <dzh961003@qq.com>
2026-07-25 01:04:25 +0800

    新增仓库分类功能及物品数量展示

--- Scripts/UI/Item.cs
+++ Scripts/UI/Item.cs
@@ -5,10 +5,12 @@ using System;
 public partial class Item : Control
 {
 	public int ID;
+    public int num;
 
 	[Export] public TextureRect[] Rarity;
 	[Export] public TextureButton BG;
     [Export] public TextureRect itemIcon;
+    [Export] public Label numLabel;
 
 
     public override void _Ready()
@@ -25,7 +27,8 @@ public partial class Item : Control
             for (int i = 0; i < Rarity.Length; i++)
             {
                 Rarity[i].Visible = false;
-            }            
+            }
+            numLabel.Text = "";
         }
 		else
 		{
@@ -39,6 +42,7 @@ public partial class Item : Control
                 Rarity[i].Visible = false;
             }
             Rarity[ConfigManager.Instance.itemDic[ID].Rarity - 1].Visible = true;
+            numLabel.Text = num.ToString();
         }
 		       
     }
--- Scripts/UI/WarehouseUI.cs
+++ Scripts/UI/WarehouseUI.cs
@@ -1,8 +1,8 @@
 using Godot;
-using Godot.Collections;
 using MyProject;
-using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class WarehouseUI : Control
 {
@@ -11,8 +11,8 @@ public partial class WarehouseUI : Control
     [Export] public TextureButton[] tabButton;
 
     private List<int> ItemID = new List<int>();
+    private Dictionary<int, int> ItemDic = new Dictionary<int, int>();
 
-    // gridLength = 每行列数，和 tscn 中 columns 保持一致
     private int gridLength = 6;
     // 最少显示的格子数量，铺满至少一整屏
     private int minNum = 42;
@@ -23,66 +23,23 @@ public partial class WarehouseUI : Control
     public override void _Ready()
 	{
 		closeBtn.Pressed += () => UIManager.Instance.HideUI(Paths.WarehouseUI);
-        //for (int i = 0; i < tabButton.Length; i++)
-        //{
-        //    int index = i; 
-        //    tabButton[i].Pressed += () => SwitchBtn(index);
-        //}
 
-        //测试用，先手动添加ID为1、2的物品
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(1);
-        ItemID.Add(2);
-        ItemID.Add(1);
-        ItemID.Add(2);
+        for (int i = 0; i < tabButton.Length; i++)
+        {
+            int index = i; 
+            tabButton[i].Pressed += () => SwitchBtn(index);
+        }
 
-        SpawnItemList(ItemID);
+        //测试用，先手动添加物品及数量
+        ItemDic.Add(1, 15);
+        ItemDic.Add(2, 18);
+        ItemDic.Add(3, 16);
+
+        ItemID = ItemDic.Keys.ToList<int>();
+
+        SpawnItemList(ItemID,0);
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
@@ -91,20 +48,28 @@ public partial class WarehouseUI : Control
     /// 根据物品ID列表，在 GridContainer 中生成物品格子。
     /// 不足 minNum 时补空格子；超过时按 gridLength 取余补齐最后一行。
     /// </summary>
-    private void SpawnItemList(List<int> ItemID)
+    private void SpawnItemList(List<int> AllItemList,int type)
     {
+        List<int> itemList = GetSortedItemID(AllItemList);
         // 当物品种类小于最小数量时，最低生成42个格子
         if (ItemID.Count <= minNum)
         {
-            for (int i = 0; i < ItemID.Count; i++)
+            int skipNum = 0;
+            for (int i = 0; i < itemList.Count; i++)
             {
+                if (ConfigManager.Instance.itemDic[itemList[i]].Type!=type && type != 0)
+                {
+                    skipNum += 1;
+                    continue;
+                }
                 var itemScene = GD.Load<PackedScene>("res://UI/item.tscn");
                 Item item = itemScene.Instantiate<Item>();
                 ItemList.AddChild(item);
-                item.ID = ItemID[i];
+                item.ID = itemList[i];
+                item.num = ItemDic[itemList[i]];
                 item.InitialItem();
             }
-            for (int i = 0; i < (minNum - ItemID.Count); i++)
+            for (int i = 0; i < (minNum+ skipNum - itemList.Count); i++)
             {
                 var itemScene = GD.Load<PackedScene>("res://UI/item.tscn");
                 Item item = itemScene.Instantiate<Item>();
@@ -116,25 +81,47 @@ public partial class WarehouseUI : Control
         // 高于最低数量时，根据取余将格子生成满
         else
         {
-            for (int i = 0; i < ItemID.Count; i++)
+            int skipNum = 0;
+            for (int i = 0; i < itemList.Count; i++)
             {
+                if (ConfigManager.Instance.itemDic[itemList[i]].Type != type && type != 0)
+                {
+                    skipNum += 1;
+                    continue;
+                }
                 var itemScene = GD.Load<PackedScene>("res://UI/item.tscn");
                 Item item = itemScene.Instantiate<Item>();
                 ItemList.AddChild(item);
                 item.ID = ItemID[i];
+                item.num = ItemDic[itemList[i]];
                 item.InitialItem();
             }
             // 补齐最后一行不满的部分
-            int remainder = ItemID.Count % gridLength;
-            int fillCount = (remainder == 0) ? 0 : (gridLength - remainder);
-            for (int i = 0; i < fillCount; i++)
+            if (itemList.Count- skipNum>=minNum)
             {
-                var itemScene = GD.Load<PackedScene>("res://UI/item.tscn");
-                Item item = itemScene.Instantiate<Item>();
-                ItemList.AddChild(item);
-                item.ID = 0;
-                item.InitialItem();
+                int remainder = (itemList.Count - skipNum) % gridLength;
+                int fillCount = (remainder == 0) ? 0 : (gridLength - remainder);
+                for (int i = 0; i < fillCount; i++)
+                {
+                    var itemScene = GD.Load<PackedScene>("res://UI/item.tscn");
+                    Item item = itemScene.Instantiate<Item>();
+                    ItemList.AddChild(item);
+                    item.ID = 0;
+                    item.InitialItem();
+                }
             }
+            else
+            {
+                for (int i = 0; i < minNum- (itemList.Count - skipNum); i++)
+                {
+                    var itemScene = GD.Load<PackedScene>("res://UI/item.tscn");
+                    Item item = itemScene.Instantiate<Item>();
+                    ItemList.AddChild(item);
+                    item.ID = 0;
+                    item.InitialItem();
+                }
+            }
+            
         }
 
         // GridContainer 在动态添加子节点后，需要手动让它重新计算自身的最小尺寸，
@@ -142,9 +129,49 @@ public partial class WarehouseUI : Control
         // 不调用的话，ScrollContainer 可能拿到的还是旧的最小尺寸，导致无法滚到底部。
         ItemList.UpdateMinimumSize();
     }
-
-    public void LoadSaveData(Dictionary data)
+    private void SwitchBtn(int index) 
     {
+        for (int i = 0; i < tabButton.Length; i++)
+        {
+            tabButton[i].TextureNormal = ResourceLoader.Load<Texture2D>("res://Assets/Images/UI/tab_inactive.png");
+        }
+        tabButton[index].TextureNormal = ResourceLoader.Load<Texture2D>("res://Assets/Images/UI/tab_active.png");
+        for (int i = 0; i < ItemList.GetChildCount(); i++)
+        {
+            ItemList.GetChild(i).QueueFree();
+        }
+        switch (index)
+        {
+            case 0:
+                SpawnItemList(ItemID, 0);
+                break;
+            case 1:
+                SpawnItemList(ItemID, 1);
+                break;
+            case 2:
+                SpawnItemList(ItemID, 2);
+                break;
+            case 3:
+                SpawnItemList(ItemID, 3);
+                break;
+            case 4:
+                SpawnItemList(ItemID, 4);
+                break;
+            default:
+                SpawnItemList(ItemID, 0);
+                break;
+        }
         
+    }
+
+    public List<int> GetSortedItemID(List<int> itemList)
+    {
+        List<int> sortedList = itemList;
+
+        return sortedList
+            .OrderBy(id => ConfigManager.Instance.itemDic[id].Type)
+            .ThenBy(id => ConfigManager.Instance.itemDic[id].Rarity)
+            .ThenBy(id => id)  // 最后按ID排序
+            .ToList();
     }
 }
