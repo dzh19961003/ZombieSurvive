8742942ccb0df60fb23de5f842c22dc11ee4253d
dzh19961003 <dzh961003@qq.com>
2026-08-12 17:01:35 +0800

    增加若干图片

--- Scripts/UI/MainUI.cs
+++ Scripts/UI/MainUI.cs
@@ -13,27 +13,26 @@ namespace MyProject
         public override void _Ready()
         {
             propertyBtn.Pressed += EnterPro;
-            sleepBtn.Pressed +=()=>
+            sleepBtn.Pressed += () =>
             {
-                if (PlayerManager.Instance.Hunger!=1)
+                CommonTips tips = new CommonTips();
+                if (PlayerManager.Instance.Hunger != 1)
                 {
-                   CommonTips tips= UIManager.Instance.ShowCommonTips("上床休息", "是否确定上床休息并结束今天？");
-                    tips.OnConfirm = () =>
+                    tips = UIManager.Instance.ShowCommonTips("上床休息", "是否确定上床休息并结束今天？");
+                }
+                else
+                {
+                    tips = UIManager.Instance.ShowCommonTips("上床休息", "你非常饥饿！现在睡觉可能就见不到明天的太阳了，是否确定睡觉？");
+                }
+                tips.OnConfirm = () =>
                     {
                         do
                         {
                             GameManager.Instance.AdvanceTime();
                         }
-                        while (GameManager.Instance.CurrentTimePeriod!=0);
-                        
+                        while (GameManager.Instance.CurrentTimePeriod != 0);
+
                     };
-                }
-                else
-                {
-                    UIManager.Instance.ShowCommonTips("上床休息", "你非常饥饿！现在睡觉可能就见不到明天的太阳了，是否确定睡觉？");
-                    GameManager.Instance.AdvanceTime();
-                }
-                
             };
         }
 
