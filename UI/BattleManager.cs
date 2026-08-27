a9263f13bfbc29b1833c32f1bd3cad208a98bba5
dzh19961003 <dzh961003@qq.com>
2026-08-27 18:28:55 +0800

    新增战斗界面及进度条功能

---
+++ UI/BattleManager.cs
@@ -0,0 +1,92 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class BattleManager : Control
+{
+    [Export] public TextureRect progressBar;
+    [Export] public TextureRect playerHead;
+    [Export] public TextureRect enemyHead;
+
+    //头像移动速度
+    private double speed = 0.4;
+    private double playerSpeed;
+    private double enemySpeed;
+    private double playerPosition;
+    private double enemyPosition;
+    private float positionBiasY;
+    private float positionBiasX;
+
+    private BattleState battleState = BattleState.Moving;
+    enum BattleState
+    {
+        Moving = 1,
+        Player = 2,
+        Enemy = 3,
+        End = 4
+    }
+    public override void _Ready()
+    {
+        positionBiasY = playerHead.Size.Y;
+        positionBiasX = playerHead.Size.X / 2;
+        playerHead.Position = progressBar.Position - new Vector2(0, positionBiasY);
+        enemyHead.Position = progressBar.Position + new Vector2(0, positionBiasY);
+        NormalizedSpeed(5, 5);
+    }
+
+
+    public override void _Process(double delta)
+    {
+        if (battleState != BattleState.Moving)
+        {
+            return;
+        }
+        float deltaF = (float)delta;
+        playerPosition += playerSpeed * deltaF;
+        enemyPosition += enemySpeed * deltaF;
+        if (playerPosition >= 1)
+        {
+            battleState = BattleState.Player;
+            PlayerTurn();
+        }
+        if (enemyPosition >= 1)
+        {
+            battleState = BattleState.Enemy;
+            EnemyTurn();
+        }
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        playerHead.Position = progressBar.Position + new Vector2((float)(playerPosition / 1) * progressBar.Size.X - positionBiasX, -positionBiasY);
+        enemyHead.Position = progressBar.Position + new Vector2((float)(enemyPosition / 1) * progressBar.Size.X - positionBiasX, positionBiasY);
+    }
+    private void NormalizedSpeed(int playerSpeedProperty, int enemySpeedProperty)
+    {
+        if (playerSpeedProperty > enemySpeedProperty)
+        {
+            playerSpeed = speed;
+            enemySpeed = Math.Round((double)enemySpeedProperty / playerSpeedProperty, 2) * speed;
+        }
+        else if (enemySpeedProperty > playerSpeedProperty)
+        {
+            enemySpeed = speed;
+            playerSpeed = Math.Round((double)playerSpeedProperty / enemySpeedProperty, 2) * speed;
+        }
+        else if (playerSpeedProperty == enemySpeedProperty)
+        {
+            playerSpeed = speed;
+            enemySpeed = 0.99 * speed;
+        }
+    }
+    private void PlayerTurn()
+    {
+        GD.Print("玩家行动");
+    }
+    private void EnemyTurn()
+    {
+        GD.Print("敌人行动");
+    }
+
+}
